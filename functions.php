76a3b0545562bd4feeb9c43821aad72c8b85d9e6
Maarten <maarten@rodekanarie.nl>
2022-01-16 18:10:07 +0100

    make footer prettier

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+//add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+//function my_theme_enqueue_styles() {
+//    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+//        array( 'parenthandle' ), 
+//        wp_get_theme()->get('Version') // this only works if you have Version in the style header
+//    );
+//}
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 function my_theme_enqueue_styles() {
@@ -13,6 +21,7 @@ function my_theme_enqueue_styles() {
         array( $parenthandle ),
         $theme->get('Version') // this only works if you have Version in the style header
     );
+    //wp_enqueue_style( 'child-style', get_stylesheet_uri());
 }
 
-
+?>
